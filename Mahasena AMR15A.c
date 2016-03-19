d9a57667a7ea0984260c1b340b73e3e225ab70e1
Pradyumn Agrawal <geniuspradyumn@gmail.com>
2016-03-19 10:48:07 +0530

    Description indentation changes

--- Mahasena AMR15A.c
+++ Mahasena AMR15A.c
@@ -1,6 +1,10 @@
-/*Kattapa, as you all know was one of the greatest warriors of his time. The kingdom of Maahishmati had never lost a battle under him (as army-chief), and the reason for that was their really powerful army, also called as Mahasena.
-Kattapa was known to be a very superstitious person. He believed that a soldier is "lucky" if the soldier is holding an even number of weapons, and "unlucky" otherwise. He considered the army as "READY FOR BATTLE" if the count of "lucky" soldiers is strictly greater than the count of "unlucky" soldiers, and "NOT READY" otherwise.
-Given the number of weapons each soldier is holding, your task is to determine whether the army formed by all these soldiers is "READY FOR BATTLE" or "NOT READY".
+/*Kattapa, as you all know was one of the greatest warriors of his time. The kingdom of Maahishmati had never lost a battle under him 
+(as army-chief), and the reason for that was their really powerful army, also called as Mahasena.
+Kattapa was known to be a very superstitious person. He believed that a soldier is "lucky" if the soldier is holding an even number of 
+weapons, and "unlucky" otherwise. He considered the army as "READY FOR BATTLE" if the count of "lucky" soldiers is strictly greater than
+ the count of "unlucky" soldiers, and "NOT READY" otherwise.
+Given the number of weapons each soldier is holding, 
+your task is to determine whether the army formed by all these soldiers is "READY FOR BATTLE" or "NOT READY".
 Note: You can find the definition of an even number here.*/
 
 #include <stdio.h>
