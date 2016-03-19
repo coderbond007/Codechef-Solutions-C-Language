f3a264a4c1552addd4099428c51edcd219c24442
Pradyumn Agrawal <geniuspradyumn@gmail.com>
2016-03-19 10:58:39 +0530

    Question Description added

--- Palindromic substrings STRPALIN.c
+++ Palindromic substrings STRPALIN.c
@@ -1,3 +1,9 @@
+/*Chef likes strings a lot but he likes palindromic strings more. 
+Today, Chef has two strings A and B, each consisting of lower case alphabets.
+Chef is eager to know whether it is possible to choose some non empty strings s1 and s2 where s1 is a substring of A, 
+s2 is a substring of B such that s1 + s2 is a palindromic string. Here '+' denotes the concatenation between the strings
+*/
+
 #include <stdio.h>
 #include <string.h>
 int main(void) 
