713d40196f28f4817f8fd3a9fb5139e445ced411
Pradyumn Agrawal <geniuspradyumn@gmail.com>
2016-04-26 22:27:23 +0530

    Problem Code: FLOW006

---
+++ Sum of Digits FLOW006.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+  	int t;
+   	scanf("%d",&t);
+   	while(t--)
+   	{
+   		int n,sum=0; 
+   		scanf("%d",&n);
+   		while(n!=0)
+   		{
+	   		sum+=n%10; n/=10;
+   		}
+    	printf("%d\n",sum);
+    }
+}
